b9c0282ea773beb37c73959774fc3974058ab676
Patrick Govus <patrickgovus99@gmail.com>
2023-10-02 16:43:56 +0100

    final cleaning added, need to comment on sql

--- database_utils.py
+++ database_utils.py
@@ -1,25 +1,38 @@
 import yaml
 import pandas as pd
-import tabula
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine
 from decouple import config
 
 CLOUD_CREDS = config('CLOUD_YAML')
 LOCAL_CREDS = config('LOCAL_YAML')
 
+# The `DatabaseConnector` class initializes a database connection using provided credentials.
 class DatabaseConnector:
     def __init__(self, creds) -> None:
         self.credentials = self.read_db_creds(creds)
         self.engine = self.init_db_engine(self.credentials)
-        # self.list_db_tables()
         pass
 
     def read_db_creds(self, creds):
+        """
+        The function reads database credentials from a file and returns the data.
+        
+        :param creds: The parameter "creds" is the file path to the credentials file that contains the
+        database credentials
+        :return: The `self.data` variable is being returned.
+        """
         with open(creds, 'r') as file:
             self.data = yaml.load(file, Loader=yaml.FullLoader)
         return self.data
 
     def init_db_engine(self, creds):
+        """
+        The function initializes a database engine using the provided credentials and returns the engine
+        object.
+        
+        :param creds: The `creds` parameter is a dictionary that contains the following keys:
+        :return: the database engine object.
+        """
         DATABASE_TYPE = 'postgresql'
         DBAPI = 'psycopg2'
         HOST = creds['HOST']
@@ -31,8 +44,20 @@ class DatabaseConnector:
         print('Database connected')
         return self.engine
 
-
     def upload_to_db(self, cleaned_dataframe: pd.DataFrame, table_name: str, creds: str):
+        """
+        The function uploads a cleaned dataframe to a PostgreSQL database table.
+        
+        :param cleaned_dataframe: A pandas DataFrame that has been cleaned and is ready to be uploaded
+        to a database
+        :type cleaned_dataframe: pd.DataFrame
+        :param table_name: The `table_name` parameter is a string that specifies the name of the table
+        in the database where you want to upload the cleaned dataframe
+        :type table_name: str
+        :param creds: The `creds` parameter is a string that represents the path or location of a file
+        containing the credentials needed to connect to the database
+        :type creds: str
+        """
         creds_formatted = self.read_db_creds(creds)
         connection = self.init_db_engine(creds_formatted)
         print('almost there')
@@ -42,8 +67,4 @@ class DatabaseConnector:
 
 if __name__ == '__main__':
     db = DatabaseConnector(creds=CLOUD_CREDS)
-    # formatted_creds = db.read_db_creds(creds=CLOUD_CREDS)
-    # db.init_db_engine(formatted_creds)
-
-    # db.upload_to_db()
     
